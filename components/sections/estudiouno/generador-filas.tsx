29b704caf5c720cc4ea84327ae6b6110b9289be4
Arturo <turriscontreras@gmail.com>
2026-02-18 17:40:01 -0600

    Se hace el ejercicio de multiplicador del 1 al 10 y e identificar numeros pares

--- components/sections/estudiouno/generador-filas.tsx
+++ components/sections/estudiouno/generador-filas.tsx
@@ -7,10 +7,11 @@ import { useState } from "react"
 
 const GeneradorFilas = () => {
   
-
+  
   const [numeroFilas, setNumeroFilas] = useState(0)
   const [listaFilas, setListaFilas] = useState<string[]>([])
 
+  //GENERADOR DE FILAS
   const generarFilas = () => {
     const listaTemporal: string[] = [] 
       for (let i = 1; i <= numeroFilas; i++) {
@@ -23,12 +24,32 @@ const GeneradorFilas = () => {
     setNumeroFilas(0)
     setListaFilas([])
   } 
+
+
+  //Multiplicar 1 al 10
+
+  const [numeroMultiplicar, setNumeroMultiplicar] = useState(0)
+  const [listaNumeros, setListaNumeros] = useState<string[]>([])
+
+  const multiplicarPares = () => {
+    const listaMultiplicaciones: string[] = []
+    for (let i = 1; i <= 10; i++) {
+      listaMultiplicaciones.push(`${i} * ${numeroMultiplicar} = ${i * numeroMultiplicar} ${(i * numeroMultiplicar) % 2 === 0 ? "(es par)" : ""}`)
+    }
+    setListaNumeros(listaMultiplicaciones)
+  }
+  
+  const resetearMultiplicar = () => {
+    setNumeroMultiplicar(0)
+    setListaNumeros([])
+  }
+  
   
   return (
     <>
+    {/*GENERADOR DE FILAS*/}
       <div className="p-6 border rounded-xl shadow-sm bg-card space-y-4">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance text-pink-300">Generador de Filas</h1>
-        
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance text-pink-300">Generador de Filas</h1>  
         <div className="flex gap-4 items-center">
           <Input 
             type="number"
@@ -37,15 +58,14 @@ const GeneradorFilas = () => {
             value={numeroFilas === 0 ? "" : numeroFilas}
             onChange={e => {
               setNumeroFilas(Number(e.target.value))
-            }}>
-          </Input>
+            }} />
           <Button onClick={generarFilas}>
               Generar
           </Button>
           <Button variant="destructive" onClick={resetNumerFilas}>
               Resetear
           </Button>
-          <div className="text-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-6 w-full">
           {listaFilas.map((fila, index) => (
             <div key={index} className="p-3 bg-muted border rounded-md text-sm font-medium animate-in fade-in slide-in-from-left-2">
               {fila}
@@ -54,6 +74,37 @@ const GeneradorFilas = () => {
           </div>
         </div>
 
+      </div>
+
+      {/* multiplicar 1 al 10*/}
+      <div className="p-6 border rounded-xl shadow-sm bg-card space-y-4">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance text-pink-300">Multiplicar pares</h1>
+        <div className="flex gap-4 items-center">
+          <Input
+            type="number"
+            placeholder="número a multiplicar"
+            className="max-w-50"
+            value={numeroMultiplicar === 0 ? "" : numeroMultiplicar}
+            onChange={ e => {
+              setNumeroMultiplicar(Number(e.target.value))
+            }}
+            />
+          <Button onClick={multiplicarPares}>
+            Multiplicar  
+          </Button>
+          <Button variant="destructive" onClick={resetearMultiplicar}>
+            Resetear
+          </Button>
+
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-6 w-full">
+            {listaNumeros.map((n, i) => (
+              <div key={i} className="p-3 bg-muted border rounded-md text-sm font-medium animate-in fade-in slide-in-from-left-2">
+                {n}
+              </div>
+            ))}
+          </div>    
+        </div>
       </div>
       
     </>
